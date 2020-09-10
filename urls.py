a32fd37ec4b3bd227a71d35909308310257d42b6
Pratyush Shivam <58632486+pratyushshivam@users.noreply.github.com>
2020-09-10 22:28:44 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,10 @@
+#File: ecommerce/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('store.urls')),
+]
